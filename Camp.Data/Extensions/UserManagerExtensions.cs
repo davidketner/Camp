ce6d5c172211fcfca6e930bc0d71bc275b6a4736
davidketner <ketnda00@upol.cz>
2018-11-03 15:27:57 +0100

    Backup pc

--- Camp.Data/Extensions/UserManagerExtensions.cs
+++ Camp.Data/Extensions/UserManagerExtensions.cs
@@ -1,10 +1,7 @@
 ﻿using Camp.Data.Entity;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace Camp.Web.Extensions
 {
